b6e4a1f9a4de0a8e3cc61da90501b290df40b991
Byeongmin Lee <bmlee531@gmail.com>
2024-05-12 09:03:16 +0900

    update: 이용약관 및 개인정보 처리방침

--- src/components/pages/PrivacyPolicy/PrivacyPolicy.tsx
+++ src/components/pages/PrivacyPolicy/PrivacyPolicy.tsx
@@ -5,7 +5,63 @@ export interface PrivacyPolicyProps {}
 export const PrivacyPolicy = ({}: PrivacyPolicyProps) => {
   return (
     <Container as="main" className="max-w-4xl p-4 mx-auto">
-      PrivacyPolicy
+      <h2 className="typography-h2">개인정보 처리방침</h2>
+
+      <br />
+
+      <br />
+
+      <h4 className="typography-h4">1. 수집하는 개인정보 항목</h4>
+      <p className="typography-p">
+        {'<Todo>'}(‘www.example.com’이하 ‘Todo) 은 다음의 목적을 위하여 개인정보를
+        처리하고 있으며, 다음의 목적 이외의 용도로는 이용하지 않습니다.
+      </p>
+      <ul className="typography-list">
+        <li>이메일 주소</li>
+        <li>성명</li>
+        <li>주소</li>
+        <li>연락처 등</li>
+        <li>개인정보의 이용 목적</li>
+      </ul>
+
+      <h4 className="typography-h4">2. 서비스 제공을 위한 연락</h4>
+      <ul className="typography-list">
+        <li>고객 지원 및 문의 응대</li>
+        <li>이벤트나 프로모션 정보 제공 등</li>
+      </ul>
+
+      <h4 className="typography-h4">2. 개인정보의 보유 및 이용 기간</h4>
+      <ul className="typography-list">
+        <li>
+          정보주체로부터 개인정보를 수집할 때 동의 받은 개인정보 보유․이용기간 또는 법령에
+          따른 개인정보 보유․이용기간 내에서 개인정보를 처리․보유합니다.
+        </li>
+        <li>회원 탈퇴 시까지 또는 목적 달성 후 즉시 파기</li>
+      </ul>
+
+      <h4 className="typography-h4">3. 정보주체의 권익침해에 대한 구제방법 </h4>
+      <p className="typography-p">
+        아래의 기관은 ToDo 과는 별개의 기관으로서, ‘ToDo` 자체적인 개인정보 불만처리,
+        피해구제 결과에 만족하지 못하시거나 보다 자세한 도움이 필요하시면 문의하여 주시기
+        바랍니다
+      </p>
+      <ul className="typography-list">
+        <strong>개인정보 침해신고센터 (한국인터넷진흥원 운영)</strong>
+
+        <li>소관업무 : 개인정보 침해사실 신고, 상담 신청</li>
+        <li>홈페이지 : privacy.kisa.or.kr</li>
+        <li>전화 : (국번없이) 118</li>
+        <li>
+          주소 : (58324) 전남 나주시 진흥길 9(빛가람동 301-2) 3층 개인정보침해신고센터
+        </li>
+      </ul>
+      <ul className="typography-list">
+        <strong>개인정보 분쟁조정위원회</strong>
+        <li>소관업무 : 개인정보 분쟁조정신청, 집단분쟁조정 (민사적 해결)</li>
+        <li>홈페이지 : www.kopico.go.kr</li>
+        <li>전화 : (국번없이) 1833-6972</li>
+        <li>주소 : (03171)서울특별시 종로구 세종대로 209 정부서울청사 4층</li>
+      </ul>
     </Container>
   );
 };
--- src/components/pages/TermsOfService/TermsOfService.tsx
+++ src/components/pages/TermsOfService/TermsOfService.tsx
@@ -5,7 +5,73 @@ export interface TermsOfServiceProps {}
 export const TermsOfService = ({}: TermsOfServiceProps) => {
   return (
     <Container as="main" className="max-w-4xl p-4 mx-auto">
-      TermsOfService
+      <h2 className="typography-h2">서비스 이용약관</h2>
+
+      <br />
+
+      <br />
+
+      <h4 className="typography-h4">1. 목적</h4>
+      <p className="typography-p mb-4">
+        본 약관은 Todo(이하 “회사”라 합니다)이 운영하는 웹사이트 ‘Todo` (www.example.com)
+        (이하 “웹사이트”라 합니다)에서 제공하는 온라인 서비스(이하 “서비스”라 한다)를
+        이용함에 있어 사이버몰과 이용자의 권리, 의무 및 책임사항을 규정함을 목적으로
+        합니다.
+      </p>
+
+      <h4 className="typography-h4">2. 용어의 정의</h4>
+      <ul className="typography-list">
+        <li>
+          “웹사이트”란 회사가 재화 또는 용역을 이용자에게 제공하기 위하여 컴퓨터 등
+          정보통신설비를 이용하여 재화 또는 용역을 거래 할 수 있도록 설정한 가상의
+          영업장을 말하며, 아울러 사이버몰을 운영하는 사업자의 의미로도 사용합니다.
+        </li>
+        <li>
+          “이용자”란 “웹사이트”에 접속하여 서비스를 이용하는 회원 및 비회원을 말합니다.
+        </li>
+        <li>
+          “회원”이라 함은 “웹사이트”에 개인정보를 제공하여 회원등록을 한 자로서,
+          “웹사이트”의 정보를 지속적으로 제공받으며, “웹사이트”이 제공하는 서비스를
+          계속적으로 이용할 수 있는 자를 말합니다.
+        </li>
+        <li>
+          “비회원”이라 함은 회원에 가입하지 않고, “웹사이트”이 제공하는 서비스를 이용하는
+          자를 말합니다.
+        </li>
+      </ul>
+
+      <h4 className="typography-h4">3. 서비스 이용 조건</h4>
+      <ul className="typography-list">
+        <li>본 서비스를 이용하기 위해서는 회원 가입이 필요합니다.</li>
+        <li>부적절한 내용의 게시물은 관리자에 의해 삭제될 수 있습니다.</li>
+      </ul>
+
+      <h4 className="typography-h4">4. 서비스 이용 제한 사항</h4>
+      <ul className="typography-list">
+        <li>타인의 개인정보를 유출하는 행위</li>
+        <li>저작권을 침해하는 게시물 작성</li>
+        <li>공공질서 및 미풍양속, 기타 사회질서를 해하는 경우</li>
+        <li>
+          범죄행위를 목적으로 하거나 기타 범죄행위와 관련된다고 객관적으로 인정되는 경우
+        </li>
+        <li>해킹 및 컴퓨터 바이러스 유포 등으로 서비스의 건전한 운영을 저해하는 경우</li>
+        <li>
+          기타 관계 법령에 위배되는 경우 및 회사가 이용자로서 부적당하다고 판단한 경우
+        </li>
+      </ul>
+
+      <h4 className="typography-h4">5. 책임 제한</h4>
+      <ul className="typography-list">
+        <li>본 서비스의 이용으로 발생한 문제에 대해 어떠한 책임도 지지 않습니다.</li>
+      </ul>
+
+      <h4 className="typography-h4">6. 약관 외 준칙</h4>
+      <p className="typography-p">
+        본 약관에 명시되지 않은 사항이 전기통신기본법, 전기통신사업법, 정보통신촉진법,
+        ‘전자상거래등에서의 소비자 보호에 관한 법률’, ‘약관의 규제에관한법률’,
+        ‘전자거래기본법’, ‘전자서명법’, ‘정보통신망 이용촉진등에 관한 법률’,
+        ‘소비자보호법’ 등 기타 관계 법령에 규정되어 있을 경우에는 그 규정을 따르도록 한다.
+      </p>
     </Container>
   );
 };
